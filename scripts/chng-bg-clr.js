1a62676ebc80fbb369eaf0a7672c143a3d662b6c
Nisha Singhal <111162548+nishaYO@users.noreply.github.com>
2023-02-07 16:48:48 +0530

    improved code and added comments

--- scripts/chng-bg-clr.js
+++ scripts/chng-bg-clr.js
@@ -1,10 +1,15 @@
-const change = document.getElementById("change-color");
+const change_btn = document.getElementById("change-color");
 
 // generating random color
 function random(number){
     return Math.floor(Math.random()*(number+1));
 }
 
-change.addEventListener('click', function () {
-    document.body.style.backgroundColor = `rgb(${random(255)},${random(255)},${random(255)})`;
-});
+// creating event handler
+function clickhandler(){
+    random_color = `rgb(${random(255)},${random(255)},${random(255)})`;
+    document.body.style.backgroundColor = random_color;
+}
+
+// creating event listener
+change_btn.addEventListener('click', clickhandler);
